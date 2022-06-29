6675c8298521f1e20cc27ec89a13922766257dce
临江仙Mac <2607881950@qq.com>
2022-06-29 20:11:38 +0800

    更新富文本

--- src/views/Editor/index.vue
+++ src/views/Editor/index.vue
@@ -27,7 +27,19 @@
           v-model="valueHtml"
           :defaultConfig="editorConfig"
           @onCreated="handleCreated"
+          @customPaste="customPaste"
+          @keyup.enter="handleEnter"
         />
+        <!-- @contextmenu.prevent -->
+        <el-tooltip
+          class="item"
+          effect="dark"
+          content="按Enter发送消息,Enter+Shift换行"
+          placement="left-start"
+          :open-delay="800"
+        >
+          <div class="btn-send" @click="sendMessage">发送</div>
+        </el-tooltip>
       </div>
     </div>
   </div>
@@ -54,7 +66,20 @@ onMounted(() => {
 // 工具栏配置
 const toolbarConfig = {
   /* 显示哪些菜单，如何排序、分组 */
-  toolbarKeys: ["emotion"],
+  toolbarKeys: [
+    "emotion", //表情
+    "uploadImage",
+    {
+      key: "group-more-style", // 必填，要以 group 开头
+      title: "更多样式", // 必填
+      iconSvg: "<svg>....</svg>", // 可选
+      menuKeys: ["through", "code", "clearStyle"], // 下级菜单 key ，必填
+    },
+  ],
+  // insertKeys: {
+  //   index: 2, // 插入的位置，基于当前的 toolbarKeys
+  //   keys: ["menu-key1", "menu-key2"],
+  // },
   /* 隐藏哪些菜单 */
   excludeKeys: [],
 };
@@ -71,6 +96,43 @@ const handleCreated = (editor) => {
 
   // 查看所有工具栏key
   console.log(editor.getAllMenuKeys());
+  console.log(editor.getConfig());
+
+};
+// 回车
+const handleEnter = () => {
+  console.log("huiche");
+};
+// 发送消息
+const sendMessage = () => {};
+// 粘贴事件
+const customPaste = (editor, event, callback) => {
+  console.log(editor);
+  console.log("ClipboardEvent 粘贴事件对象", event);
+  // const html = event.clipboardData.getData("text/html"); // 获取粘贴的 html
+  // const text = event.clipboardData.getData("text/plain"); // 获取粘贴的纯文本
+  // const rtf = event.clipboardData.getData("text/rtf"); // 获取 rtf 数据（如从 word wsp 复制粘贴）
+  // // console.log(html);
+  // console.log(text);
+  // console.log(rtf);
+
+  if (event.clipboardData && event.clipboardData.items) {
+    const items = event.clipboardData.items;
+    console.log(items);
+    // for (const { index, key } of items) {
+    //   console.log(key);
+    // }
+  }
+
+  // 自定义插入内容
+  // editor.insertText("xxx");
+
+  // 返回 false ，阻止默认粘贴行为
+  event.preventDefault();
+  callback(false); // 返回值（注意，vue 事件的返回值，不能用 return）
+
+  // 返回 true ，继续默认的粘贴行为
+  // callback(true)
 };
 
 // 组件销毁时，及时销毁编辑器
@@ -86,11 +148,22 @@ onBeforeUnmount(() => {
   height: 206px;
   .toolbar {
     // border-bottom: 1px solid #ccc
+    // 表情
+    ::v-deep .w-e-bar-item .w-e-drop-panel {
+      top: -292px;
+      margin: 0;
+    }
   }
   .editor-content {
     // height: 500px;
     height: calc(100% - 40px) !important;
     overflow-y: hidden;
+    ::v-deep .w-e-text-container p {
+      margin: 0;
+    }
+    ::v-deep .w-e-text-placeholder {
+      top: 2px;
+    }
   }
 }
 .list-container {
@@ -121,6 +194,7 @@ onBeforeUnmount(() => {
   height: 60px;
 }
 .scrollbar-list {
+  background: #fff;
   height: calc(100% - 60px);
 }
 .scrollbar-item {
@@ -133,5 +207,20 @@ onBeforeUnmount(() => {
   border-radius: 4px;
   background: var(--el-color-primary-light-9);
   color: var(--el-color-primary);
+}
+.btn-send {
+  cursor: pointer;
+  position: absolute;
+  bottom: 8px;
+  right: 16px;
+  padding: 6px 6px 4px 4px;
+  line-height: 20px;
+  width: 60px;
+  background-color: #f44336;
+  color: #fff;
+  border: 1px solid #e7e7e7;
+  font-size: 14px;
+  border-radius: 3px;
+  text-align: center;
 }
 </style>
