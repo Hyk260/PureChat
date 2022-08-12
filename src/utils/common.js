ec70deec71d022f20a08c3754bc61b20b04d4852
临江仙Mac <2607881950@qq.com>
2022-08-12 11:09:06 +0800

    功能函数

---
+++ src/utils/common.js
@@ -0,0 +1,45 @@
+/** 切换主题风格
+ * @param {string}  appearance light || dark
+ */
+export function changeAppearance(appearance = "auto") {
+  if (appearance === "auto" || appearance === undefined) {
+    appearance = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
+  document.body.setAttribute("data-theme", appearance);
+  // let dom = document.querySelector('meta[name="theme-color"]');
+  // dom?.setAttribute("content", appearance === "dark" ? "#222" : "#fff");
+}
+
+/**
+ * 格式化文件大小
+ * @param {Number}  bytes
+ */
+export function bytesToSize(bytes) {
+  const marker = 1024; // Change to 1000 if required
+  const decimal = 2; // Change as required
+  const kiloBytes = marker;
+  const megaBytes = marker * marker;
+  const gigaBytes = marker * marker * marker;
+
+  const lang = store.state.settings.lang;
+
+  if (bytes < kiloBytes) return bytes + (lang === "en" ? " Bytes" : "字节");
+  else if (bytes < megaBytes) {
+    return (bytes / kiloBytes).toFixed(decimal) + " KB";
+  } else if (bytes < gigaBytes) {
+    return (bytes / megaBytes).toFixed(decimal) + " MB";
+  } else return (bytes / gigaBytes).toFixed(decimal) + " GB";
+}
+
+export function randomNum(minNum, maxNum) {
+  switch (arguments.length) {
+    case 1:
+      return parseInt(Math.random() * minNum + 1, 10);
+    case 2:
+      return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+    default:
+      return 0;
+  }
+}
