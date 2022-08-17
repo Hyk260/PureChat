aee405b31700b6b4536ecffe12342f9a832f3f4d
黄泳康 <2607881950@qq.com>
2022-08-17 17:13:18 +0800

    打包清除console.log配置
    Signed-off-by: 黄泳康 <2607881950@qq.com>

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
+const plugins = []
+if (process.env.NODE_ENV === 'production') {
+  // exclude(忽略)
+  plugins.push(['transform-remove-console', { 'exclude': ['error', 'warn'] }])
+}
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: [
+    "@vue/cli-plugin-babel/preset"
+  ],
+  plugins
 };
