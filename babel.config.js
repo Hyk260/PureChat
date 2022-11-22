7d3f0983f897cd86db0a704438effa38d3929606
临江仙 <2607881950@qq.com>
2022-11-22 15:52:36 +0800

    :wrench: 修改配置文件

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,9 @@
-const plugins = []
-if (process.env.NODE_ENV === 'production') {
+const plugins = [];
+if (process.env.NODE_ENV === "production") {
   // exclude(忽略)
-  plugins.push(['transform-remove-console', { 'exclude': ['error', 'warn'] }])
+  plugins.push(["transform-remove-console", { exclude: ["error", "warn"] }]);
 }
 module.exports = {
-  presets: [
-    "@vue/cli-plugin-babel/preset"
-  ],
-  plugins
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins,
 };
