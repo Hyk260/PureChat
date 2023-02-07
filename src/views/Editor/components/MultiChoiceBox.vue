21938c53d6c62bc2952d163db9db1583f14ba982
临江仙 <2607881950@qq.com>
2023-02-07 21:32:56 +0800

    # 服务端api接口 REST API

--- src/views/Editor/components/MultiChoiceBox.vue
+++ src/views/Editor/components/MultiChoiceBox.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="Checkbox-style" id="svgDown" v-if="showCheckbox">
-    <el-button type="primary" @click="shutdown">关闭</el-button>
-    <el-button type="primary" @click="test">测试</el-button>
+  <div class="checkbox-style" id="svgDown" v-if="showCheckbox">
+    <!-- 合并转发 -->
+    <div class="btn" @click="mergeForward">
+      <svg-icon iconClass="mergeForward" />
+    </div>
+    <!-- 逐条转发 -->
+    <div class="btn" @click="aQuickForward">
+      <svg-icon iconClass="aQuickForward" />
+    </div>
+    <!-- 关闭 -->
+    <div class="btn" @click="shutdown">
+      <svg-icon iconClass="close" />
+    </div>
   </div>
 </template>
 
@@ -25,15 +35,18 @@ export default defineComponent({
   },
   methods: {
     ...mapMutations(["SET_CHEC_BOX"]),
+    mergeForward() {
+      console.log(this.forwardData);
+    },
+    aQuickForward() {
+      console.log(this.forwardData);
+    },
     shutdown() {
       this.SET_CHEC_BOX(false);
       const el = document.getElementsByClassName("check-btn");
       for (let i = 0; i < el.length; i++) {
         el[i].checked = false;
       }
-    },
-    test() {
-      console.log(this.forwardData);
     },
   },
   setup(props, { attrs, emit, expose, slots }) {
@@ -49,10 +62,22 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.Checkbox-style {
+.checkbox-style {
   position: relative;
   z-index: 1;
   background: #fff;
   height: 206px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.btn {
+  width: 35px;
+  height: 35px;
+  background: #e5e6eb;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
