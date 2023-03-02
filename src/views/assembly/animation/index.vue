8dd946d9b8932612f5700d737500b56da19c01df
临江仙 <2607881950@qq.com>
2023-03-02 20:33:44 +0800

    updata

---
+++ src/views/assembly/animation/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+import { reactive, onMounted, onBeforeUnmount } from "vue";
+import { mapGetters, mapState, mapMutations, mapActions } from "vuex";
+export default {
+  name: "Componentname",
+  components: {},
+  computed: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
