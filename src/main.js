0472b84ad35fcf100a21bcdb5a1b4abb48eadbf1
黄泳康 <2607881950@qq.com>
2024-05-30 09:16:28 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import "tailwindcss/tailwind.css"
 
 import './assets/main.css'
 
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
