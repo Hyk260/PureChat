2bd8cfe3154718d87756a8952c30771ffa2fb26a
临江仙 <2607881950@qq.com>
2024-07-06 11:42:50 +0800

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
+    spacing: Array.from({ length: 1000 }).reduce((map, _, index) => {
+      map[index] = `${index}px`;
+      return map;
+    }, {}),
     extend: {},
   },
   plugins: [],
