0a05c68e65d3f79d98a1993b3dca1c1002cbcc19
yongkang <2607881950@qq.com>
2024-08-26 14:59:11 +0800

    添加图片查看器功能，优化用户交互体验

---
+++ src/views/components/ImageViewer/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-image-viewer
+    v-if="dialog"
+    :initialIndex="0"
+    :url-list="[url]"
+    :zoom-rate="1.2"
+    :max-scale="7"
+    :min-scale="0.2"
+    @close="setDialog(false)"
+  />
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import emitter from "@/utils/mitt-bus";
+import { useBoolean } from "@/utils/hooks/index";
+
+const url = ref('https://ljx-1307934606.cos.ap-beijing.myqcloud.com/xuax.jpg')
+const [dialog,setDialog] = useBoolean()
+
+onMounted(() => {
+  emitter.on('onImageViewer', (src) => {
+    // url.value = src
+    setDialog(true)
+  })
+})
+</script>
+
+<style lang="scss" scoped></style>
