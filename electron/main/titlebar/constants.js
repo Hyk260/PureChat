4fb6d5b4b1f86c1423aceb3df3c1a45c733d41d4
yongkang <2607881950@qq.com>
2024-08-26 16:06:06 +0800

    删除electron主标题栏相关文件

--- electron/main/titlebar/constants.js
+++
@@ -1,3 +0,0 @@
-export const TITLE_BAR_CHANNEL = 'uikit:titlebar'
-export const TITLE_BAR_MAXIMIZE_REPLY_CHANNEL = 'uikit:titlebar:maximize-reply'
-export const TITLE_BAR_FULLSCREEN_REPLY_CHANNEL = 'uikit:titlebar:fullscreen-reply'
