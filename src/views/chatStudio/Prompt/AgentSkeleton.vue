4c1eaeff106bb3ad97d215034d213383d94515e5
yongkang <2607881950@qq.com>
2024-10-22 16:50:52 +0800

    feat: 重构聊天室骨架屏，优化用户体验

---
+++ src/views/chatStudio/Prompt/AgentSkeleton.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-skeleton class="skeleton" v-for="item in 9" :key="item">
+    <template #template>
+      <el-skeleton-item variant="image" class="h-40 w-40 ml-auto" />
+      <div>
+        <el-skeleton-item variant="p" />
+        <el-skeleton-item variant="p" />
+        <div>
+          <el-skeleton-item variant="text" style="margin-right: 16px" />
+          <el-skeleton-item variant="text" style="width: 30%" />
+        </div>
+      </div>
+    </template>
+  </el-skeleton>
+</template>
+
+<style lang="scss" scoped>
+.skeleton {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding: 16px;
+}
+</style>
