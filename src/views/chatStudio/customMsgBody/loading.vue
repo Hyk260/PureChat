aeb5187c0ac6e3f0a3ce7f429e714db779141fa0
yongkang <2607881950@qq.com>
2024-11-27 09:33:29 +0800

    build: 添加ldrs和snabbdom依赖，调整package.json结构

--- src/views/chatStudio/customMsgBody/loading.vue
+++ src/views/chatStudio/customMsgBody/loading.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <svg-icon iconClass="Loader" />
+    <!-- <l-orbit size="25" speed="1.5" color="black"></l-orbit> -->
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+// import "ldrs/orbit";
+</script>
 
 <style lang="scss" scoped>
 .svg-icon {
