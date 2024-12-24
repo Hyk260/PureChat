227f07d8494311c7e78ba611091449658e1f9eb9
yongkang <2607881950@qq.com>
2024-12-24 14:49:18 +0800

    feat: 重构路由守卫，合并进度条和路由守卫逻辑，移除冗余文件

--- src/router/guard/index.js
+++ src/router/guard/index.js
@@ -1,12 +1,40 @@
 import { isElectron } from "@/utils/common";
-import { createRouteGuard } from './route';
-import { createProgressGuard } from './progress';
+import { ACCESS_TOKEN } from "@/constants/index";
+import { localStg } from "@/utils/storage";
+
+
+function createProgressGuard(router) {
+  router.beforeEach((to, from, next) => {
+    window.NProgress?.start?.();
+    next();
+  });
+  router.afterEach((to) => {
+    window.NProgress?.done?.();
+  });
+}
+
+function createRouteGuard(router) {
+  router.beforeEach((to, from, next) => {
+    if (from.path === to.path) return;
+    const token = localStg.get(ACCESS_TOKEN);
+    if (token) {
+      next();
+    } else {
+      if (__LOCAL_MODE__) next()
+      if (to.path !== "/login") {
+        next({ path: "/login" });
+      } else {
+        next();
+      }
+    }
+  });
+}
 
 /**
  * Router guard
  * @param router - Router instance
  */
 export function createRouterGuard(router) {
-  !isElectron && createProgressGuard(router)
+  if (!isElectron) createProgressGuard(router)
   createRouteGuard(router)
 }
