cdd6490dcf4ced09d29525a0dbbf4d2597b487f0
临江仙 <2607881950@qq.com>
2025-01-26 15:57:01 +0800

    feat: 优化聊天实例初始化，移除动态导入模块的异步处理，简化代码结构

--- src/utils/IM/im-sdk/tim.js
+++ src/utils/IM/im-sdk/tim.js
@@ -1,31 +1,36 @@
+import TencentCloudChat from "@/utils/IM/chat/index";
+import GroupModule from "@tencentcloud/chat/modules/group-module.js";
+import SignalingModule from "@tencentcloud/chat/modules/signaling-module.js";
+import TIMUploadPlugin from "tim-upload-plugin";
+
 import LocalChat from "@/utils/IM/chat/local";
 
 // 聊天实例
-let chat = {}
+let chat = {};
 
 // 初始化聊天实例
-async function initChat() {
+function initChat() {
   // 本地模式
   if (__LOCAL_MODE__) {
     chat = LocalChat.create({});
   } else {
     // 动态导入腾讯云IM SDK相关模块
-    const [
-      { default: TencentCloudChat },
-      { default: GroupModule },
-      { default: SignalingModule }, 
-      { default: TIMUploadPlugin }
-    ] = await Promise.all([
-      import("@/utils/IM/chat/index"),
-      import("@tencentcloud/chat/modules/group-module.js"),
-      import("@tencentcloud/chat/modules/signaling-module.js"),
-      import("tim-upload-plugin")
-    ]);
+    // const [
+    //   { default: TencentCloudChat },
+    //   { default: GroupModule },
+    //   { default: SignalingModule },
+    //   { default: TIMUploadPlugin },
+    // ] = await Promise.all([
+    //   import("@/utils/IM/chat/index"),
+    //   import("@tencentcloud/chat/modules/group-module.js"),
+    //   import("@tencentcloud/chat/modules/signaling-module.js"),
+    //   import("tim-upload-plugin"),
+    // ]);
 
     // 获取环境变量配置
     const appid = import.meta.env.VITE_IM_SDK_APPID;
     const level = import.meta.env.VITE_LOG_LEVEL;
-    
+
     // SDK初始化配置
     const options = {
       SDKAppID: Number(appid),
@@ -52,4 +57,4 @@ async function initChat() {
 }
 
 // 导出初始化后的聊天实例
-export default await initChat();
+export default initChat();
