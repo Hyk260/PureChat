dc2427e32bfa9ec80a26776e4af7a8f227b65724
临江仙 <2607881950@qq.com>
2025-03-19 21:22:09 +0800

    feat: 优化 RobotOptions 和 ShareModal 组件的布局，添加滚动条以改善用户体验

--- src/views/chatStudio/components/RobotOptions.vue
+++ src/views/chatStudio/components/RobotOptions.vue
@@ -10,103 +10,107 @@
     :close-on-click-modal="false"
     :before-close="handleClose"
   >
-    <div>
-      <ul class="container">
-        <!-- prompt -->
-        <DragPrompt :prompt="maskData.prompt" @handlePrompt="handlePrompt" />
-        <li class="container-item py-10 flex-bc" v-for="item in modelData" :key="item.ID">
-          <div class="flex flex-col gap-5">
-            <div class="title">{{ item.Title }}</div>
-            <div class="subTitle">
-              <Markdown :marked="item.SubTitle" />
-              <!-- <small>{{ item.SubTitle }} </small> -->
+    <div class="container">
+      <el-scrollbar>
+        <div class="px-10">
+          <!-- prompt -->
+          <DragPrompt :prompt="maskData.prompt" @handlePrompt="handlePrompt" />
+          <div class="container-item py-10 flex-bc" v-for="item in modelData" :key="item.ID">
+            <div class="flex flex-col gap-5">
+              <div class="title">{{ item.Title }}</div>
+              <div class="subTitle">
+                <Markdown :marked="item.SubTitle" />
+                <!-- <small>{{ item.SubTitle }} </small> -->
+              </div>
             </div>
-          </div>
-          <!-- 模型 -->
-          <div v-if="item.options">
-            <div class="flex gap-8 flex-col">
-              <el-select
-                multiple
-                collapse-tags
-                collapse-tags-tooltip
-                :max-collapse-tags="10"
-                v-model="item.collapse"
-                append-to="body"
-                @clear="handleClear"
-                @remove-tag="handleRemoveTag"
-              >
-                <el-option
-                  v-for="models in item.options.chatModels"
-                  :key="models.id"
-                  :label="models.displayName"
-                  :value="models.id"
+            <!-- 模型 -->
+            <div v-if="item.options">
+              <div class="flex gap-8 flex-col">
+                <el-select
+                  multiple
+                  collapse-tags
+                  collapse-tags-tooltip
+                  :max-collapse-tags="10"
+                  v-model="item.collapse"
+                  append-to="body"
+                  @clear="handleClear"
+                  @remove-tag="handleRemoveTag"
                 >
-                  <div class="bot-model-option">
-                    <div
-                      class="bot-avatar flex-c h-full"
-                      :class="reIcon(item, models) ? models.icon : robotIcon"
-                    >
-                      <svg-icon v-if="reIcon(item, models)" :local-icon="models.icon" />
-                      <svg-icon v-else :local-icon="robotIcon" />
+                  <el-option
+                    v-for="models in item.options.chatModels"
+                    :key="models.id"
+                    :label="models.displayName"
+                    :value="models.id"
+                  >
+                    <div class="bot-model-option">
+                      <div
+                        class="bot-avatar flex-c h-full"
+                        :class="reIcon(item, models) ? models.icon : robotIcon"
+                      >
+                        <svg-icon v-if="reIcon(item, models)" :local-icon="models.icon" />
+                        <svg-icon v-else :local-icon="robotIcon" />
+                      </div>
+                      <div class="flex flex-col h-full gap-4">
+                        <span>{{ models.displayName || models.id }}</span>
+                        <span class="text-models">{{ models.id }}</span>
+                      </div>
                     </div>
-                    <div class="flex flex-col h-full gap-4">
-                      <span>{{ models.displayName || models.id }}</span>
-                      <span class="text-models">{{ models.id }}</span>
-                    </div>
+                  </el-option>
+                </el-select>
+                <div class="flex-bc">
+                  <div class="text-[#999]">
+                    共 {{ modelCount(item.collapse.length) }} 个模型可用
                   </div>
-                </el-option>
-              </el-select>
-              <div class="flex-bc">
-                <div class="text-[#999]">共 {{ modelCount(item.collapse.length) }} 个模型可用</div>
-                <!-- <div>
+                  <!-- <div>
                   <el-tooltip :content="modelTooltipText()" placement="top" v-if="isOllama()">
                     <el-icon class="refresh" @click="onRefresh()">
                       <Refresh />
                     </el-icon>
                   </el-tooltip>
                 </div> -->
+                </div>
               </div>
             </div>
-          </div>
-          <div class="range" v-else-if="isRange(item.ID)">
-            <span class="break-normal min-w-18">
-              {{ item.defaultValue }}
-            </span>
-            <input
-              v-model="item.defaultValue"
-              :min="item.min"
-              :max="item.max"
-              :step="item.step"
-              type="range"
-            />
-          </div>
-          <div class="number" v-else-if="['max_tokens'].includes(item.ID)">
-            <input v-model="item.defaultValue" :min="item.min" :max="item.max" type="number" />
-          </div>
-          <div class="input flex-ac" v-else-if="['token', 'openaiUrl'].includes(item.ID)">
-            <el-tooltip content="配置教程" placement="top">
-              <span
-                v-if="item.doubt && ['token'].includes(item.ID)"
-                class="flex mr-5 cursor-pointer"
-              >
-                <el-icon @click="toUrl(item.doubt)"><QuestionFilled /></el-icon>
+            <div class="range" v-else-if="isRange(item.ID)">
+              <span class="break-normal min-w-18">
+                {{ item.defaultValue }}
               </span>
-              <!-- ollama -->
-              <span v-else-if="item.doubt && isOllama()" class="flex mr-5 cursor-pointer">
-                <el-icon @click="toUrl(item.doubt)"><QuestionFilled /></el-icon>
-              </span>
-              <span v-else> </span>
-            </el-tooltip>
-            <el-input
-              v-model="item.defaultValue"
-              :ref="(e) => inputRef(e, item.ID)"
-              :placeholder="item.Placeholder"
-              :type="item.ID === 'token' ? 'password' : 'text'"
-              :show-password="item.ID === 'token'"
-            />
+              <input
+                v-model="item.defaultValue"
+                :min="item.min"
+                :max="item.max"
+                :step="item.step"
+                type="range"
+              />
+            </div>
+            <div class="number" v-else-if="['max_tokens'].includes(item.ID)">
+              <input v-model="item.defaultValue" :min="item.min" :max="item.max" type="number" />
+            </div>
+            <div class="input flex-ac" v-else-if="['token', 'openaiUrl'].includes(item.ID)">
+              <el-tooltip content="配置教程" placement="top">
+                <span
+                  v-if="item.doubt && ['token'].includes(item.ID)"
+                  class="flex mr-5 cursor-pointer"
+                >
+                  <el-icon @click="toUrl(item.doubt)"><QuestionFilled /></el-icon>
+                </span>
+                <!-- ollama -->
+                <span v-else-if="item.doubt && isOllama()" class="flex mr-5 cursor-pointer">
+                  <el-icon @click="toUrl(item.doubt)"><QuestionFilled /></el-icon>
+                </span>
+                <span v-else> </span>
+              </el-tooltip>
+              <el-input
+                v-model="item.defaultValue"
+                :ref="(e) => inputRef(e, item.ID)"
+                :placeholder="item.Placeholder"
+                :type="item.ID === 'token' ? 'password' : 'text'"
+                :show-password="item.ID === 'token'"
+              />
+            </div>
           </div>
-        </li>
-      </ul>
+        </div>
+      </el-scrollbar>
     </div>
     <template #footer>
       <span>
@@ -349,9 +353,8 @@ emitter.on("onRobotBox", (data) => {
 }
 .container {
   overflow: hidden;
-  overflow-y: auto;
-  max-height: 80vh;
-  padding: 0 10px;
+  // height: 480px;
+  height: 68vh;
   .container-item {
     color-scheme: light;
     user-select: none;
