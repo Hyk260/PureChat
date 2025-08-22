3c5ca2415901947d8d4def5b0d4f1b877e4a8c09
yongkang <2607881950@qq.com>
2025-08-22 17:18:33 +0800

    style: 格式化代码并更新配置文件

--- eslint.config.js
+++ eslint.config.js
@@ -156,6 +156,7 @@ export default defineConfig([
       globals: {
         ...globals.browser,
         ...globals.node,
+        Env: "readonly",
       },
     },
     plugins: {
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,13 +1,15 @@
-import type { App } from 'vue';
 import { createPinia } from "pinia";
-import { resetSetupStore } from './plugins';
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+import { resetSetupStore } from "./plugins";
+
+import type { App } from "vue";
 
 export function setupStore(app: App) {
   const store = createPinia();
   store.use(resetSetupStore);
-  store.use(piniaPluginPersistedstate)
+  store.use(piniaPluginPersistedstate);
   app.use(store);
 }
 
-export * from './modules/index';
+export * from "./modules/index";
