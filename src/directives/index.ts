ddfadcd3a96f4954a051264c1afb6e552140d3bc
临江仙 <2607881950@qq.com>
2025-10-19 21:54:02 +0800

    refactor(context-menu): remove v-contextmenu dependency and associated styles, streamline directive setup

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,12 +1,8 @@
-import { directive } from "v-contextmenu"
-
 import type { App } from "vue"
 
 // import { optimize } from "./optimize";
-import "v-contextmenu/dist/themes/default.css"
 
 /** setup custom vue directives. - [安装自定义的vue指令] */
 export function setupDirectives(app: App) {
-  app.directive("contextmenu", directive as any)
   // app.directive("optimize", optimize)
 }
